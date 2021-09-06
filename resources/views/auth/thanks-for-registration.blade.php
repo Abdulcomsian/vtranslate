3c7b5d8d1980ad93b82f18075f0f19d4106b9aea
obaidjani <muhammad.obaid@dazaran.com>
2021-09-06 15:57:00 +0500

    Larave auth,login,register,vefified email,email notification,validation,migration and sedder are done

---
+++ resources/views/auth/thanks-for-registration.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master' ,['page_title' => 'Register'])
+@section('content')
+<section id="payment-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="payment-content">
+                    <i class="fa fa-check" aria-hidden="true"></i>
+                    <h2>Thanks for Registration!</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,48 +13,65 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('screens.home');
-});
-Route::get('/search-freelancer', function () {
-    return view('screens.freelancer.search-freelancer');
-});
-Route::get('/top-freelancer', function () {
-    return view('screens.freelancer.top-freelancer');
-});
-Route::get('/search-agencies', function () {
-    return view('screens.agencies.search-agencies');
-});
-Route::get('/top-agencies', function () {
-    return view('screens.agencies.top-agencies');
-});
-Route::get('/forums', function () {
-    return view('screens.forums');
-});
-Route::get('/contact-us', function () {
-    return view('screens.contact-us');
-});
-Route::get('/profile', function () {
-    return view('screens.profile');
-});
 Route::get('/login', function () {
     return view('screens.auth.login');
 });
 Route::get('/register', function () {
     return view('screens.auth.register');
 });
-Route::get('/about-us', function () {
-    return view('screens.about-us');
-});
-Route::get('/privacy-policy', function () {
-    return view('screens.privacy-policy');
-});
-Route::get('/term-condition', function () {
-    return view('screens.term-condition');
-});
-Route::get('/how-it-works', function () {
-    return view('screens.how-it-works');
-});
-Route::get('/faq', function () {
-    return view('screens.faq');
-});
+
+Auth::routes(['verify' => true]);
+Route::get('email/verify', [App\Http\Controllers\Auth\VerificationController::class,'show'])->name('verification.notice');
+
+//route group
+
+    Route::get('/', function () {
+    return view('screens.home');
+    });
+    Route::get('/search-freelancer', function () {
+        return view('screens.freelancer.search-freelancer');
+    });
+    Route::get('/top-freelancer', function () {
+        return view('screens.freelancer.top-freelancer');
+    });
+    Route::get('/search-agencies', function () {
+        return view('screens.agencies.search-agencies');
+    });
+    Route::get('/top-agencies', function () {
+        return view('screens.agencies.top-agencies');
+    });
+    Route::get('/forums', function () {
+        return view('screens.forums');
+    });
+    Route::get('/contact-us', function () {
+        return view('screens.contact-us');
+    });
+
+    Route::get('/about-us', function () {
+        return view('screens.about-us');
+    });
+    Route::get('/privacy-policy', function () {
+        return view('screens.privacy-policy');
+    });
+    Route::get('/term-condition', function () {
+        return view('screens.term-condition');
+    });
+    Route::get('/how-it-works', function () {
+        return view('screens.how-it-works');
+    });
+    Route::get('/faq', function () {
+        return view('screens.faq');
+    });
+
+Route::middleware(['verified','auth'])->group(function()
+    {
+        Route::get('/profile', function () {
+            return view('screens.profile');
+        });
+    });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Show this page after verifying email address
+Route::view('/thanks-for-registration','auth.thanks-for-registration');
+//Show thanks page after successfull registeration of user
+Route::view('/successfully-registered','auth.login-success')->name('login.success');
